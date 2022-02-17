f3220702be5348e4d620f9d44cef63e3e84dcfd9
CryptoXSS <91107078+CryptoXSS@users.noreply.github.com>
2022-02-18 00:45:11 +0100

    Update DarkMatter.py

--- DarkMatter.py
+++ DarkMatter.py
@@ -49,18 +49,17 @@ url = f"http://{str(ip)}"
 print("[>>>] RS-28 Sarmat [<<<]")
 sleep(1)
 
+print ('\x1b[6;30;42m' + 'Success!' + '\x1b[0m')
+print ('\x1b[6;30;42m' + 'Success!' + '\x1b[0m')
+print ('\x1b[6;30;42m' + 'Success!' + '\x1b[0m')
+print ('\x1b[6;30;42m' + 'Success!' + '\x1b[0m')
+print ('\x1b[6;30;42m' + 'Success!' + '\x1b[0m')
+print ('\x1b[6;30;42m' + 'Success!' + '\x1b[0m')
+
 def send2attack():
   for i in range(5000): #Poder Mágico
     mp = multiprocessing.Process(target=attack)
     mp.setDaemon = False
     mp.start() #Magic Starts
 
- print ('\x1b[6;30;42m' + 'Success!' + '\x1b[0m')
-print ('\x1b[6;30;42m' + 'Success!' + '\x1b[0m')
-print ('\x1b[6;30;42m' + 'Success!' + '\x1b[0m')
-print ('\x1b[6;30;42m' + 'Success!' + '\x1b[0m')
-print ('\x1b[6;30;42m' + 'Success!' + '\x1b[0m')
-print ('\x1b[6;30;42m' + 'Success!' + '\x1b[0m')
- 
-    
 send2attack()
