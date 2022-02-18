623ee8e1c1bed472713158d120c58763e1d1fdf3
CryptoXSS <91107078+CryptoXSS@users.noreply.github.com>
2022-02-18 01:16:21 +0100

    Update DarkMatter.py

--- DarkMatter.py
+++ DarkMatter.py
@@ -61,5 +61,12 @@ def send2attack():
     mp = multiprocessing.Process(target=attack)
     mp.setDaemon = False
     mp.start() #Magic Starts
-
+    
+print ('\x1b[6;30;42m' + 'Success!' + '\x1b[0m')
+print ('\x1b[6;30;42m' + 'Success!' + '\x1b[0m')
+print ('\x1b[6;30;42m' + 'Success!' + '\x1b[0m')
+print ('\x1b[6;30;42m' + 'Success!' + '\x1b[0m')
+print ('\x1b[6;30;42m' + 'Success!' + '\x1b[0m')
+print ('\x1b[6;30;42m' + 'Success!' + '\x1b[0m')
+    
 send2attack()
