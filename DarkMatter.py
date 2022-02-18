e4fccd138c0da873450cadf9f9ddbecc3b1b9887
CryptoXSS <91107078+CryptoXSS@users.noreply.github.com>
2022-02-18 01:19:06 +0100

    Update DarkMatter.py

--- DarkMatter.py
+++ DarkMatter.py
@@ -11,7 +11,7 @@ print("Sistema detectado: ", sysOS)
 
 if sysOS == "Linux":
   try:
-    os.system("ulimit -n 1000000")
+    os.system("ulimit -n 8192")
   except Exception as e:
     print(e)
     print("No se pudo iniciar el script")
