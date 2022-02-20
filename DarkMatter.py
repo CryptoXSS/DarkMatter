b708b41b517ae39a08dd9dddc508bd8c0043e177
CryptoXSS <91107078+CryptoXSS@users.noreply.github.com>
2022-02-20 15:38:53 +0100

    Update DarkMatter.py

--- DarkMatter.py
+++ DarkMatter.py
@@ -5,6 +5,28 @@ import multiprocessing
 import random
 import platform
 
+import sys
+
+def progressbar(it, prefix="", size=60, file=sys.stdout):
+    count = len(it)
+    def show(j):
+        x = int(size*j/count)
+        file.write("%s[%s%s] %i/%i\r" % (prefix, "#"*x, "."*(size-x), j, count))
+        file.flush()
+        file.write("\n")
+    show(0)
+    for i, item in enumerate(it):
+        yield item
+        show(i+1)
+        file.write("\n")
+    file.flush()
+    
+import time
+
+for i in progressbar(range(15), "Computing: ", 40):
+    time.sleep(0.1)
+    
+    
 print("Detecting System...")
 sysOS = platform.system()
 print("System detected: ", sysOS)
