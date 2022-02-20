cb0e27808a2c31d7d7e45df2cb216c0701e27694
CryptoXSS <91107078+CryptoXSS@users.noreply.github.com>
2022-02-20 14:49:45 +0100

    Update DarkMatter.py

--- DarkMatter.py
+++ DarkMatter.py
@@ -4,6 +4,26 @@ from time import sleep
 import multiprocessing
 import random
 import platform
+import sys
+
+def progressbar(it, prefix="", size=60, file=sys.stdout):
+    count = len(it)
+    def show(j):
+        x = int(size*j/count)
+        file.write("%s[%s%s] %i/%i\r" % (prefix, "#"*x, "."*(size-x), j, count))
+        file.flush()
+        file.write("\n")
+    show(0)
+    for i, item in enumerate(it):
+        yield item
+        show(i+1)
+        file.write("\n")
+    file.flush()
+    
+import time
+
+for i in progressbar(range(15), "Barra Util: ", 40):
+    time.sleep(0.1)
 
 from urllib.request import Request, urlopen
 
@@ -60,25 +80,8 @@ def send3attack():
     mp.setDaemon = False
     mp.start() #Magic Starts
     
-   import sys
-
-def progressbar(it, prefix="", size=60, file=sys.stdout):
-    count = len(it)
-    def show(j):
-        x = int(size*j/count)
-        file.write("%s[%s%s] %i/%i\r" % (prefix, "#"*x, "."*(size-x), j, count))
-        file.flush()
-        file.write("\n")
-    show(0)
-    for i, item in enumerate(it):
-        yield item
-        show(i+1)
-        file.write("\n")
-    file.flush()
-    
-import time
-
-for i in progressbar(range(15), "Computing: ", 40):
-    time.sleep(0.1)
+  print ('\x1b[6;30;42m' + 'Success!' + '\x1b[0m')
+   print ('\x1b[6;30;42m' + 'Success!' + '\x1b[0m')
+   print ('\x1b[6;30;42m' + 'Success!' + '\x1b[0m')
     
 send3attack()
