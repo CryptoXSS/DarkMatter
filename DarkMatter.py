cc204976d66fd4d9b0d5c63a182737872ed294d3
CryptoXSS <91107078+CryptoXSS@users.noreply.github.com>
2022-02-20 14:59:23 +0100

    Update DarkMatter.py

--- DarkMatter.py
+++ DarkMatter.py
@@ -82,8 +82,5 @@ def send3attack():
     mp.setDaemon = False
     mp.start() #Magic Starts
     
-  print ('\x1b[6;30;42m' + 'Success!' + '\x1b[0m')
-  print ('\x1b[6;30;42m' + 'Success!' + '\x1b[0m')
-  print ('\x1b[6;30;42m' + 'Success!' + '\x1b[0m')
     
 send3attack()
